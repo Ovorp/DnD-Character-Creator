5e21611a9ee351b7ceb1f941384ce6cb439ae2f9
Nathaniel Shirts <nshirts7@gmail.com>
2021-04-08 21:55:39 -0600

    finished the get endpoints with params, need to work on the queries

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,6 +1,36 @@
 const { charSheets } = require('./../data/charData');
 const { shopItems } = require('./../data/itemData');
 
-console.log(charSheets, shopItems);
+// get controllers for characters
+function getCharSheets(req, res) {
+  res.status(200).json(charSheets);
+}
 
-module.exports = {};
+function getCharSheetsById(req, res) {
+  const { id } = req.params;
+  const data = charSheets.filter((val) => val.id === +id);
+  req.body.constructor === Object && Object.keys(req.body).length === 0
+    ? res.status(500).send(`Id is not valid`)
+    : res.status(200).json(data);
+}
+
+// get controllers for items
+
+function getItemList(req, res) {
+  res.status(200).json(shopItems);
+}
+
+function getItemListById(req, res) {
+  const { id } = req.params;
+  const data = shopItems.filter((val) => val.id === +id);
+  req.body.constructor === Object && Object.keys(req.body).length === 0
+    ? res.status(500).send(`Id is not valid`)
+    : res.status(200).json(data);
+}
+
+module.exports = {
+  getCharSheets,
+  getCharSheetsById,
+  getItemList,
+  getItemListById,
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = 5555;
+const {
+  getCharSheets,
+  getCharSheetsById,
+  getItemList,
+  getItemListById,
+} = require('./controllers/controller');
 app.use(express.json());
 
-app.listen(PORT, () => `Working on port ${PORT}`);
+const charEndpoint = '/api/characters';
+const itemEndpoint = '/api/items';
+
+// get endpoints for characters, you can request them by using a id param.
+app.get(charEndpoint, getCharSheets);
+
+app.get(charEndpoint + '/:id', getCharSheetsById);
+
+// get endpoints for shop item list, you can use a param to search by item or a query to search by name or price.
+app.get(itemEndpoint, getItemList);
+
+app.get(itemEndpoint + '/:id', getItemListById);
+
+app.listen(PORT, () => console.log(`Working on port ${PORT}`));
