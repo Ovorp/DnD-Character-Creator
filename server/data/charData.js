e584465489d3e9a3b376d70b9ca56b3194820c5b
Nathaniel Shirts <nshirts7@gmail.com>
2021-04-13 10:34:25 -0600

    added bio lorem

--- server/data/charData.js
+++ server/data/charData.js
@@ -10,7 +10,8 @@ const charSheets = [
     wis: 13,
     cha: 9,
     inventory: [],
-    bio: '',
+    bio:
+      'Aute cillum labore nisi est aliquip ipsum in. Aute reprehenderit adipisicing ex aliqua aliquip tempor et eiusmod aliquip laborum veniam culpa nulla. Fugiat aliqua sint ex nisi velit. Fugiat commodo sit est minim nisi non. Lorem voluptate nostrud et id ullamco minim cupidatat dolor labore duis. Irure mollit ullamco in ea ad mollit deserunt.',
     avatar:
       'https://db4sgowjqfwig.cloudfront.net/campaigns/178338/assets/813777/Tortle.png?1514853655',
     gold: 642,
@@ -26,7 +27,8 @@ const charSheets = [
     wis: 14,
     cha: 10,
     inventory: [],
-    bio: '',
+    bio:
+      'Amet nisi dolor laboris deserunt in est. Occaecat quis nulla nisi ad id exercitation. Aute id magna est labore qui velit pariatur sint ullamco qui sit labore non. Culpa id culpa pariatur consequat amet laborum voluptate consequat magna ex excepteur. Aute enim aliquip officia nostrud duis id irure nisi elit cupidatat veniam consequat minim velit. Et officia deserunt officia et mollit id. Officia sit elit occaecat amet consequat minim Lorem eiusmod.',
     avatar: 'https://pbs.twimg.com/media/DkVH93jW0AAzXwP.png',
     gold: 451,
   },
@@ -41,7 +43,8 @@ const charSheets = [
     wis: 10,
     cha: 18,
     inventory: [],
-    bio: '',
+    bio:
+      'Aliquip commodo ut ea eiusmod laborum dolore aliqua aliquip nulla proident officia. Ut voluptate amet qui consequat esse est quis minim. Reprehenderit fugiat duis laboris sit exercitation ad consectetur laborum qui magna aliqua. Tempor labore velit quis enim qui nostrud irure ad aute magna magna ad occaecat cillum. Et dolore proident ipsum cillum velit sunt labore laborum sit laborum pariatur adipisicing exercitation. Proident non id Lorem occaecat velit et tempor.',
     avatar: 'https://pbs.twimg.com/media/DMJbPTJVQAA5u3A.jpg',
     gold: 566,
   },
@@ -56,7 +59,8 @@ const charSheets = [
     wis: 12,
     cha: 16,
     inventory: [],
-    bio: '',
+    bio:
+      'Do enim id sit reprehenderit ad aliquip sint deserunt in anim. Ad reprehenderit nisi nulla ipsum qui tempor reprehenderit. Amet magna laborum enim reprehenderit ipsum non ea enim fugiat. Incididunt deserunt minim labore do enim consectetur laboris.',
     avatar:
       'https://64.media.tumblr.com/f2626f1bf3721b1753aa8c428c9434e5/tumblr_oiem29UTd11qkpz2go1_1280.png',
     gold: 200,
